dbce310317b568e5cba507ce1888fb5e0b02f65e
eb3ev <ebv2905@gmail.com>
2021-03-27 13:44:45 +0000

    Reformatted directory and cleaned up code with argument checking

--- lsr.py
+++ lsr.py
@@ -31,3 +31,39 @@ def view_data_segments(xs, ys):
     plt.set_cmap('Dark2')
     plt.scatter(xs, ys, c=colour)
     plt.show()
+
+def seperate_options_and_filenames(args):
+    available_args = ["--plot"]
+    fnames = []
+    opts = []
+
+    for arg in args:
+        if arg in available_args:
+            opts.append(arg)
+        elif os.path.isfile(arg) and arg.endswith(".csv"):
+            fnames.append(arg)
+        else:
+            print("Invalid Arg: " + arg)
+
+    return opts, fnames
+
+
+def calc_total_reconstruction_error(xs, ys):
+    return 0
+
+def main(argv):
+    opts, fnames = seperate_options_and_filenames(argv)
+
+    if "--plot" in opts:
+        isPlot = True
+    else:
+        isPlot = False
+
+    for fname in fnames:
+        xs, ys = load_points_from_file(fname)
+        if isPlot:
+            view_data_segments(xs, ys)
+        
+        print(calc_total_reconstruction_error(xs, ys))
+
+main(sys.argv[1:])
