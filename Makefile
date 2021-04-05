b2f055add3415b0fe7365380b9408f7310544041
eb3ev <ebv2905@gmail.com>
2021-04-05 21:54:54 +0100

    added more to makefile and cleaned up code a bit

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all: basic adv noise
 
+allPlot: basicPlot advPlot noisePlot
+
 basic: basic1 basic2 basic3 basic4
 
 basic1:
@@ -35,3 +37,39 @@ noise2:
 
 noise3:
 	python lsr.py train_data/noise_3.csv
+
+basicPlot: basic1Plot basic2Plot basic3Plot basic4Plot
+
+basic1Plot:
+	python lsr.py train_data/basic_1.csv --plot
+
+basic2Plot:
+	python lsr.py train_data/basic_2.csv --plot
+
+basic3Plot:
+	python lsr.py train_data/basic_3.csv --plot
+
+basic4Plot:
+	python lsr.py train_data/basic_4.csv --plot
+
+advPlot: adv1Plot adv2Plot adv3Plot
+
+adv1Plot:
+	python lsr.py train_data/adv_1.csv --plot
+
+adv2Plot:
+	python lsr.py train_data/adv_2.csv --plot
+
+adv3Plot:
+	python lsr.py train_data/adv_3.csv --plot
+
+noisePlot: noise1Plot noise2Plot noise3Plot
+
+noise1Plot:
+	python lsr.py train_data/noise_1.csv --plot
+
+noise2Plot:
+	python lsr.py train_data/noise_2.csv --plot
+
+noise3Plot:
+	python lsr.py train_data/noise_3.csv --plot
--- lsr.py
+++ lsr.py
@@ -14,7 +14,6 @@ def load_points_from_file(filename):
     points = pd.read_csv(filename, header=None)
     return points[0].values, points[1].values
 
-
 def view_data_segments(xs, ys):
     """Visualises the input file with each segment plotted in a different colour.
     Args:
@@ -32,7 +31,34 @@ def view_data_segments(xs, ys):
     plt.scatter(xs, ys, c=colour)
     plt.show()
 
-def seperate_options_and_filenames(args):
+def _add_polynomial_term(X, degree):
+    return np.column_stack((X**degree, X))
+
+def _add_bias(X):
+    return np.column_stack((np.ones(X.shape), X))
+
+def _w_h(X, Y):
+    return np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+
+def square_err(y, y_h):
+    return np.sum((y - y_h)**2).mean()
+
+def total_err(isPlot, xs, ys):
+    xe = _add_bias(xs)
+    a, b = _w_h(xe, ys)
+    yh = a + b * xs
+    xmin = xs.min()
+    xmax = xs.max()
+    ymin = a + b * xmin
+    ymax = a + b * xmax
+    if isPlot:
+        fig, ax = plt.subplots()
+        ax.scatter(xs, ys)
+        ax.plot([xmin, xmax], [ymin, ymax], c="#FF5955")
+        plt.show()
+    return square_err(ys, yh)
+
+def seperate_opts_and_fnames(args):
     available_args = ["--plot"]
     fnames = []
     opts = []
@@ -47,29 +73,8 @@ def seperate_options_and_filenames(args):
 
     return opts, fnames
 
-def least_squares_matrix(X, Y):
-    return np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
-
-def square_error(y, y_h):
-    return np.sum((y - y_h) ** 2)
-
-def calc_total_reconstruction_error(isPlot, xs, ys):
-    xe = np.column_stack((np.ones(xs.shape), xs))
-    a, b = least_squares_matrix(xe, ys)
-    y_h = a + b * xs
-    xmin = xs.min()
-    xmax = xs.max()
-    ymin = a + b * xmin
-    ymax = a + b * xmax
-    if isPlot:
-        fig, ax = plt.subplots()
-        ax.scatter(xs, ys)
-        ax.plot([xmin, xmax], [ymin, ymax], c="#FF5955")
-        plt.show()
-    return square_error(ys, y_h)
-
 def main(argv):
-    opts, fnames = seperate_options_and_filenames(argv)
+    opts, fnames = seperate_opts_and_fnames(argv)
 
     if "--plot" in opts:
         isPlot = True
@@ -78,6 +83,6 @@ def main(argv):
 
     for fname in fnames:
         xs, ys = load_points_from_file(fname)
-        print(calc_total_reconstruction_error(isPlot, xs, ys))
+        print(total_err(isPlot, xs, ys))
 
 main(sys.argv[1:])
